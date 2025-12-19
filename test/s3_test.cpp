da176748ba9ba59d029ad008002726e713c01310
Cristian Gutiérrez <ggcristian@icloud.com>
2025-12-19 09:12:59 +0100

    Skip tests that require to hit MinIO

--- test/s3_test.cpp
+++ test/s3_test.cpp
@@ -1,30 +1,41 @@
 #include "gtest/gtest.h"
 #include <s3cpp/s3.hpp>
 
-// #include "S3Client.h"
-//
-// int main() {
-//     S3Client client("access_key", "secret_key", "us-east-1");
-//
-//     // Upload object
-//     client.put_object("my-bucket", "path/to/file.txt", "Hello, S3!");
-//
-//     // Download object
-//     std::string content = client.get_object("my-bucket", "path/to/file.txt");
-//
-//     // List objects
-//     auto objects = client.list_objects("my-bucket", "path/");
-//     for (const auto& obj : objects) {
-//         std::cout << obj.key << " - " << obj.size << " bytes\n";
-//     }
-//
-//     // Delete object
-//     client.delete_object("my-bucket", "path/to/file.txt");
-//
-//     return 0;
-// }
-
-TEST(S3, Hello) {
+TEST(S3, ListObjectsNoPrefix) {
     S3Client client("minio_access", "minio_secret");
-		client.list_objects("my-bucket", "path/to/file.txt");
+    try {
+        client.list_objects("my-bucket");
+    } catch (const std::exception& e) {
+        const std::string emsg = e.what();
+        if (emsg == "libcurl error: Could not connect to server" || emsg == "libcurl error: Couldn't connect to server") {
+            GTEST_SKIP_("Skipping MinIOBasicRequest: Server not up");
+        }
+        throw;
+    }
+}
+
+TEST(S3, ListObjectsFilePrefix) {
+    S3Client client("minio_access", "minio_secret");
+    try {
+        client.list_objects("my-bucket", "path/to/file.txt");
+    } catch (const std::exception& e) {
+        const std::string emsg = e.what();
+        if (emsg == "libcurl error: Could not connect to server" || emsg == "libcurl error: Couldn't connect to server") {
+            GTEST_SKIP_("Skipping MinIOBasicRequest: Server not up");
+        }
+        throw;
+    }
+}
+
+TEST(S3, ListObjectsDirPrefix) {
+    S3Client client("minio_access", "minio_secret");
+    try {
+        client.list_objects("my-bucket", "path/to/");
+    } catch (const std::exception& e) {
+        const std::string emsg = e.what();
+        if (emsg == "libcurl error: Could not connect to server" || emsg == "libcurl error: Couldn't connect to server") {
+            GTEST_SKIP_("Skipping MinIOBasicRequest: Server not up");
+        }
+        throw;
+    }
 }
